68e0d44bc391f0205ec000ab2e85e7070733286e
Brennan Fee <brennan.git@mailbots.org>
2023-05-30 21:58:12 -0500

    chore: Linting, adding shebang to script

--- setup_env.sh
+++ setup_env.sh
@@ -3,12 +3,4 @@
 # Remember to source this file (source setup_env.sh or . setup_env.sh) rather
 # than executing it ./setup_env.sh.
 
-# POSIX strict mode (may produce issues in sourced scenarios)
-set -o errexit
-set -o nounset
-#set -o xtrace # same as set -x, turn on for debugging
-
-IFS=$(printf '\n\t')
-# END POSIX strict mode
-
 export ANSIBLE_INVENTORY_DNS_DOMAIN="_ansible.bfee.org"
